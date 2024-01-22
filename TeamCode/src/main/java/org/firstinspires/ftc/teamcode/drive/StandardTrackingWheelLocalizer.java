262757c2ecab1c4879386e87168e42bb0c173910
sharkie <sharkiecodes@gmail.com>
2024-01-22 20:30:31 +0100

    odometrie waardes ingevuld.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -32,8 +32,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.73; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 12.99; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 3.94; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.385826772; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 6.1023622047; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
